b3da4a6dd4ec12359036b94229f98fb996b785cf
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2016-02-22 16:17:58 +0100

    Summary rendering template with led pictures and correct background

--- src/app/components/summary/summary.controller.js
+++ src/app/components/summary/summary.controller.js
@@ -15,7 +15,12 @@
     vm.creationDate = 1452787656155;
     vm.goToAmpio = goToAmpio;
     
-    
+    vm.label1 = vm.AppState.template6 ? vm.AppState.template6.elements[0].label : '';
+    vm.label2 = vm.AppState.template6 ? vm.AppState.template6.elements[1].label : '';
+    vm.label3 = vm.AppState.template6 ? vm.AppState.template6.elements[2].label : '';
+    vm.label4 = vm.AppState.template6 ? vm.AppState.template6.elements[3].label : '';
+    vm.label5 = vm.AppState.template6 ? vm.AppState.template6.elements[4].label : '';
+    vm.label6 = vm.AppState.template6 ? vm.AppState.template6.elements[5].label : '';
     
     function goToAmpio() {
        $window.location.href = 'http://ampio.com.pl/';
