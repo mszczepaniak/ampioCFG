51191b9081e6aa0abafc2e496528c9476af2b067
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2016-02-23 17:40:30 +0100

    Install angular formly

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('ampio', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'mgcrea.ngStrap', 'toastr','ngDragDrop']);
+    .module('ampio', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'mgcrea.ngStrap', 'toastr','ngDragDrop','angular-formly']);
 
 })();
