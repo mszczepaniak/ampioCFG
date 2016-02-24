0bdd9fd3623de0fba0e590a4978cc550abb4c220
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2016-02-24 17:49:11 +0100

    WIP personalData

--- src/app/components/personalData/personalData.controller.js
+++ src/app/components/personalData/personalData.controller.js
@@ -9,12 +9,43 @@
   /** @ngInject */
   function PersonalDataController($location, $window, AppState) {
     var vm = this;
+
+    // funcation assignment
+    vm.goToAmpio = goToAmpio;
+    vm.onSubmit = onSubmit;
+
     vm.AppState = AppState.getAppState();
     console.log('APP STATE personal data page')
     console.log(vm.AppState)
     
     vm.creationDate = 1452787656155;
-    vm.goToAmpio = goToAmpio;
+    
+    // variable assignment
+    vm.model = {
+        firstName: 'Wpisz imię'
+    }
+    vm.fields = [
+        {
+            type: 'input',
+            key: 'firstName',
+            templateOptions: {
+                label: 'First Name'
+            }
+        },
+        {
+            template: '<hr />'
+        }
+    ];
+    
+    // copy fields because formly adds data to them 
+    vm.originalFields = angular.copy(vm.fields);
+    
+    // function definition
+    function onSubmit() {
+        alert(JSON.stringify(vm.model), null, 2)
+    }
+    
+    
     
     function goToAmpio() {
        $window.location.href = 'http://ampio.com.pl/';
